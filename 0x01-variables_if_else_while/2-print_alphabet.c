459dee3575698f46fa7acc2cd502903da5c1523c
esraaashraf023 <esraaashraf780567@gmail.com>
2023-03-17 16:36:35 -0700

    test2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char Cukur = 'a';
+while (Cukur <= 'z')
+{
+putchar(Cukur);
+Cukur++;
+}
+putchar('\n');
+return (0);
+}
