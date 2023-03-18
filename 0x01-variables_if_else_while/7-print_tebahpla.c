5d2ba20d92a4b1c3a2a5a76defe785b030c8c6f2
esraaashraf023 <esraaashraf780567@gmail.com>
2023-03-17 17:20:12 -0700

    ALLA

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char Cukur = 'z';
+while (Cukur >= 'a')
+{
+putchar(Cukur);
+Cukur--;
+}
+putchar('\n');
+return (0);
+}
