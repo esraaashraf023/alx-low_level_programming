5ea78b2acca94fa6de3e5b60f61db0cab80ed111
esraaashraf023 <esraaashraf780567@gmail.com>
2023-03-17 17:07:44 -0700

    teast

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int C = 0;
+while (C < 10)
+{
+printf("%d", C);
+C++;
+}
+printf("\n");
+return (0);
+}
