a8615d57aac94bc57f6becb27fcce00d6ef527e0
esraaashraf023 <esraaashraf780567@gmail.com>
2023-03-21 13:58:02 -0700

    tast1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry Point
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+char a = 'a';
+
+while (a <= 'z')
+{
+_putchar(a);
+a++;
+}
+_putchar('\n');
+}
