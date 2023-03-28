3e48cbb67540ba8f6f689a45d8179e617cfe9817
esraaashraf023 <esraaashraf780567@gmail.com>
2023-03-27 18:40:10 -0700

    hj

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ *
+ * @s: contains the string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+char *start, *end, c;
+int i, count;
+int length = 0;
+
+for (i = 0; s[i]; i++)
+{
+length++;
+}
+
+count = length;
+
+start = s;
+end = s;
+
+
+for (i = 0; i < count - 1; i++)
+{
+end++;
+}
+
+
+for (i = 0; i < count / 2; i++)
+{
+
+
+c = *end;
+*end = *start;
+*start = c;
+
+
+start++;
+end--;
+}
+}
