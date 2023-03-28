14b77ae22574cbdc5e32af3e4be7591acc11adfa
esraaashraf023 <esraaashraf780567@gmail.com>
2023-03-27 18:28:04 -0700

    ser

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value of the variable to 98
+ *
+ * @n: contains the value
+ *
+ * Return: 0
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
