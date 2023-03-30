ac8fbf8730ce720af7591cd38b90da07dade21fe
esraaashraf023 <esraaashraf780567@gmail.com>
2023-03-30 07:05:02 -0700

    dfg

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,9 +15,7 @@ char *_strcat(char *dest, char *src)
 
 	i = 0;
 	while (dest[i] != '\0')
-	{
 		i++;
-	}
 	j = 0;
 	while (src[i]  != '\0')
 	{
@@ -25,6 +23,7 @@ char *_strcat(char *dest, char *src)
 		i++;
 		j++;
 	}
+
 	dest[i] = '\0';
 
 	return (dest);
