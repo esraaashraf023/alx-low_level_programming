26665a45a17905a09587e61bd6dd343304a0a700
esraaashraf023 <esraaashraf780567@gmail.com>
2023-04-03 19:44:07 -0700

    dfg

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - that sets the value of a pointer to a char
+ * @s: ironman
+ * @to: pointer
+ * Return: return 0
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
