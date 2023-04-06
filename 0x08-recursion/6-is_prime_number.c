83bf95a61d4619c044846dd38055202aa108051b
esraaashraf023 <esraaashraf780567@gmail.com>
2023-04-06 15:16:23 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,17 +6,24 @@
  * @n: input
  * Return: void
  */
-int pre_prime_number(int n, int j);
-int is_prime_number(int n)
+int pre_prime_number(int n, int j)
 {
-	return (pre_prime_number(n, 2));
+	if (n % j == 0 && n == j)
+		return (1);
+	else if (n % j == 0)
+		return (0);
+	else
+		return (pre_prime_number(n, j + 1));
 }
-{
 /**
  * is_prime_number - the input integer
  * @n: input
- * Return: Return 0
+ * Return: void
  */
 int is_prime_number(int n)
-	return (pre_prime_number(n, 2));
-{	
+{
+	if (n == -1 || n == 1)
+		return (0);
+	else
+		return (pre_prime_number(n, 2));
+}
