ba003aada1852adef1b6adb6144387b7768af394
esraaashraf023 <esraaashraf780567@gmail.com>
2023-04-05 19:53:21 -0700

    sd

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * is_prime_number - returns 1 if the input integer
+ * @j: input
+ * @n: input
+ * Return: void
+ */
+int pre_prime_number(int n, int j);
+int is_prime_number(int n)
+{
+	return (pre_prime_number(n, 2));
+}
+{
+/**
+ * is_prime_number - the input integer
+ * @n: input
+ * Return: Return 0
+ */
+int is_prime_number(int n)
+	return (pre_prime_number(n, 2));
+{	
