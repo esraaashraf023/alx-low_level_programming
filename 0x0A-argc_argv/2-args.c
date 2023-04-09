f3ef4b475af71afd3df63923e23373c43e87b392
esraaashraf023 <esraaashraf780567@gmail.com>
2023-04-09 13:27:39 -0700

    gh

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,14 +3,15 @@
 
 /**
  * main -  prints all arguments it receives
- * @agrc: argumemt count
- * @agrv: argument vector
+ * @argc: argumemt count
+ * @argv: argument vector
  *
- * Retyrn: 0
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
 	int a;
+
 	for (a = 0; a < argc; a++)
 	{
 		printf("%s\n", argv[a]);
