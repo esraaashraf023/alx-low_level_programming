ea99bbf32af83485e030132919728359545cd3ee
esraaashraf023 <esraaashraf780567@gmail.com>
2023-04-09 10:10:20 -0700

    0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program that prints its name
+ * @agrc: index number
+ * @agrv: value
+ *
+ * Return: void
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+		printf("%s\n", *argv);
+		return (0);
+}
