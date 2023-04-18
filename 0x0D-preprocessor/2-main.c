804af426a24ed00fb63a7ccd6a3b1682b597f3bc
esraaashraf023 <esraaashraf780567@gmail.com>
2023-04-17 20:00:41 -0700

    ffg

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
-#include "0-object_like_macro.h"
-#include "0-object_like_macro.h"
 
 /**
-* main - prints the name of the file 
-* 
-* Return: 0
-*/
+ * main - prints the name of the file 
+ * 
+ * Return: 0
+ */
 int main(void)
 {
-int r;
-
-r = 98 + SIZE;
-printf("%d\n", r);
-return (0);
+	
+	printf("%s\n", __FILE_);
+	return (0);
 }
 
