13373ad4e6d6b94f1fba1af30a2050ee8593719a
esraaashraf023 <esraaashraf780567@gmail.com>
2023-10-06 06:59:48 -0700

    add file 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,20 +8,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-        hash_table_t *n_table;
+	hash_table_t *n_table;
 
-        if (!size)
-                return (NULL);
-        n_table = malloc(sizeof(hash_table_t));
-        if (!n_table)
-                return (NULL);
+	if (!size)
+		return (NULL);
+	n_table = malloc(sizeof(hash_table_t));
+	if (!n_table)
+		return (NULL);
 
-        n_table->size = size;
-        n_table->array = malloc(sizeof(hash_node_t *) * size);
-        if (!n_table->array)
-        {
-                free(n_table);
-                        return (NULL);
-        }
-        return (n_table);
+	n_table->size = size;
+	n_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!n_table->array)
+	{
+		free(n_table);
+		return (NULL);
+	}
+	return (n_table);
 }
